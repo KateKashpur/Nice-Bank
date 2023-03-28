da4597e530c391dd17fa7d1023d8e867b147c041
Kate Kashpur <128703073+KateKashpur@users.noreply.github.com>
2023-03-28 22:33:48 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Header = () => {
+    return <header className="header">
+    <img src="https://img.freepik.com/free-vector/head-bird-colorful-modern-logo-gradient_487879-805.jpg?size=338&ext=jpg&ga=GA1.1.2092267208.1679932458&semt=sph" />
+  </header>
+}
+export default Header; 
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Profile = () => {
+    return   <div className="content">
+    <div>
+      <img src="https://th.bing.com/th/id/OIP.timIjBFdAVRmBHxvXndHpQHaFj?w=235&h=180&c=7&r=0&o=5&pid=1.7" />
+    </div>
+    <div><img src="https://img.freepik.com/free-photo/water-surface-with-ripples-drop-falling_23-2147608325.jpg?size=626&ext=jpg&ga=GA1.2.2092267208.1679932458&semt=sph"/></div>
+    <div>
+      my posts
+      <div>New posts</div>
+    </div>
+    <div>
+      <div>post1</div>
+      <div>post2</div>
+    </div>
+  </div>
+}
+export default Profile; 
