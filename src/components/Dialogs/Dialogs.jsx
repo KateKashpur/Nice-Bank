a090f6f665c13b7fb9858a813167b439d133e720
Kate <mekachefuka@gmail.com>
2023-03-30 19:49:47 +0200

    added files Dialogs.module.css and Dialogs.jsx added dialogues and messages

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,21 @@ import col from './Dialogs.module.css'
 
 const Dialogs = (props) => {
  return (
-    <div>
-        Dialogs
+    <div className={col.dialogs}>
+        <div className={col.dialogsItems}>
+            <div className={col.active}>Nikitosik</div>
+            <div className={col.dialog}>Dimon</div>
+            <div className={col.dialog}>Ira</div>
+            <div className={col.dialog}>Sergey</div>
+            <div className={col.dialog}>Alla</div>
+            <div className={col.dialog}>Lena</div>
+            <div className={col.dialog}>Ruslan</div>
+        </div>
+        <div className={col.messages}>
+        <div className={col.message}>Hi! ^_^</div>
+        <div className={col.message}>How are you?</div>
+        <div className={col.message}>That nice!</div>
+        </div>
     </div>
  )
 }
