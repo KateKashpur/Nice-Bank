43a36cbdd9b45ae595abc0dff06c68914ef42bd6
Kate <mekachefuka@gmail.com>
2023-03-31 10:15:11 +0200

    added changes to dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import col from './Dialogs.module.css'
+import React from "react";
+import col from "./Dialogs.module.css";
+import { NavLink } from "react-router-dom";
 
 const Dialogs = (props) => {
- return (
+  return (
     <div className={col.dialogs}>
-        <div className={col.dialogsItems}>
-            <div className={col.active}>Nikitosik</div>
-            <div className={col.dialog}>Dimon</div>
-            <div className={col.dialog}>Ira</div>
-            <div className={col.dialog}>Sergey</div>
-            <div className={col.dialog}>Alla</div>
-            <div className={col.dialog}>Lena</div>
-            <div className={col.dialog}>Ruslan</div>
+      <div className={col.dialogsItems}>
+        <div className={col.active}>
+          <NavLink to="dialog/1">Nikitosik</NavLink>
         </div>
-        <div className={col.messages}>
+        <div className={col.dialog}>
+          <NavLink to="dialog/2">Dimon</NavLink>
+        </div>
+        <div className={col.dialog}>
+          <NavLink to="dialog/3">Ira</NavLink>
+        </div>
+        <div className={col.dialog}>
+          <NavLink to="dialog/4">Sergey</NavLink>
+        </div>
+        <div className={col.dialog}>
+          <NavLink to="dialog/5">Alla</NavLink>
+        </div>
+        <div className={col.dialog}>
+          <NavLink to="dialog/6">Lena</NavLink>
+        </div>
+        <div className={col.dialog}>
+          <NavLink to="dialog/7">Ruslan</NavLink>
+        </div>
+      </div>
+      <div className={col.messages}>
         <div className={col.message}>Hi! ^_^</div>
         <div className={col.message}>How are you?</div>
         <div className={col.message}>That nice!</div>
-        </div>
+      </div>
     </div>
- )
-}
-export default Dialogs; 
+  );
+};
+export default Dialogs;
