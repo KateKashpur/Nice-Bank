8058a7f14b3ee8f40a6b8afccfbe2cb55c42c021
Kate <mekachefuka@gmail.com>
2023-03-31 21:46:26 +0200

    added aray to messages and posts

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,10 +16,29 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
+  let dialogsData = [
+    { id: 1, name: "Nikitosik" },
+    { id: 2, name: "Dimon" },
+    { id: 3, name: "Ira" },
+    { id: 4, name: "Sergey" },
+    { id: 5, name: "Alla" },
+    { id: 6, name: "Lena" },
+    { id: 7, name: "Ruslan" },
+  ];
+
+  let messagesData = [
+    { id: 1, message: "Hi! ^_^itosik" },
+    { id: 2, message: "How are yoy?" },
+    { id: 3, message: "YO" },
+    { id: 4, message: "YO" },
+    { id: 5, message: "YO" },
+    { id: 6, message: "YO" },
+  ];
+
   return (
     <div className={col.dialogs}>
       <div className={col.dialogsItems}>
-        <DialogItem name="Nikitosik" id="1" />
+        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
         <DialogItem name="Dimon" id="2" />
         <DialogItem name="Ira" id="3" />
         <DialogItem name="Sergey" id="4" />
@@ -28,10 +47,10 @@ const Dialogs = (props) => {
         <DialogItem name="Ruslan" id="7" />
       </div>
       <div className={col.messages}>
-        <Message message="Hi! ^_^" />
-        <Message message="YO" />
-        <Message message="YO" />
-        <Message message="YO" />
+        <Message message={messagesData[0].message} />
+        <Message message={messagesData[1].message} />
+        <Message message={messagesData[2].message} />
+        <Message message={messagesData[3].message} />
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,11 @@ import col from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+  let postsData = [
+    { id: 1, message: "Hi, how are you?", likesKount: 11 },
+    { id: 2, message: "I`ts my first post", likesKount: 20 },
+  ];
+
   return (
     <div className={col.postsBlock}>
       <h3>My posts</h3>
@@ -15,8 +20,14 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={col.posts}>
-        <Post message="Hi, how are you?" likesCount="11" />
-        <Post message="I'ts my first post" likesCount="20" />
+        <Post
+          message={postsData[0].message}
+          likesCount={postsData[0].likesKount}
+        />
+        <Post
+          message={postsData[1].message}
+          likesCount={postsData[1].likesKount}
+        />
       </div>
     </div>
   );
