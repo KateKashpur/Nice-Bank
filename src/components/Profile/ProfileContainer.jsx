a636ce2adb150831642e69e1367f2ff649f900bc
Kate <mekachefuka@gmail.com>
2023-06-27 21:46:34 +0200

    85 - hook, useEffect. fix mistake at status

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -26,7 +26,7 @@ class ProfileContainer extends React.Component {
         <Profile
           {...this.props}
           profile={this.props.profile}
-          status={this.props.staus}
+          status={this.props.status}
           updateStatus={this.props.updateStatus}
         />
       </div>
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React,{useState}from "react";
 import col from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
-import ProfileStatus from "./ProfileStatus"
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -19,7 +19,7 @@ const ProfileInfo = (props) => {
   </div>
       <div className={col.descriptionBlock}>
         <img className={col.avatar} src={props.profile.photos.small} alt={alt_descriptionBlock} />
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
       </div>
     </div>
   );
