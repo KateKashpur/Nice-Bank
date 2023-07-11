54268c68750138d637e609b56d2b292a91cc940a
Kate <mekachefuka@gmail.com>
2023-07-11 14:06:30 +0200

    git pages

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ let AppContainer = compose(
 
 const SamuraiApp = (props) => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <AppContainer />
       </Provider>
