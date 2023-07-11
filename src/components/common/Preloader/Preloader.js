b022f506a288dfd25f74d1bb68393677a51eca3f
Kate <mekachefuka@gmail.com>
2023-07-11 18:48:23 +0200

    preloader

--- src/components/common/Preloader/Preloader.js
+++ src/components/common/Preloader/Preloader.js
@@ -1,13 +1,12 @@
 import React from "react";
 import preloader from "../../../assets/images/preloader.gif";
 
-
 let Preloader = () => {
-    return(
-        <div>
-    <img src={preloader} alt={'Preloader'} />
+  return (
+    <div>
+      <img src={preloader} alt={"Preloader"} />
     </div>
-    )
-}
+  );
+};
 
 export default Preloader;
